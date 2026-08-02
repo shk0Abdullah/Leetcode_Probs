ca7e2c825eb9ba934482d67491ee356213e21fc0
ABDULLAH AMJAD <sheikhabdullahamjadofficial@gmail.com>
2026-08-02 10:38:08 +0500

    Time: 5 ms (93.05%), Space: 32.3 MB (47.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        set_nums = set()
-        for i in nums: 
-            if i in set_nums:
-                return True
-            else: 
-                set_nums.add(i)
+        # bad solution quadratic complexity
+        # set_nums = set()
+        # for i in nums: 
+        #     if i in set_nums:
+        #         return True
+        #     else: 
+        #         set_nums.add(i)
+        # return False
+        # Linear Complexity
+        if len(set(nums)) != len(nums):
+            return True
         return False
