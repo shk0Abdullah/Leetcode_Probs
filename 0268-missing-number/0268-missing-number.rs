5c0a5774fabffccfb6812cdaaf4563c4c3c3f808
ABDULLAH AMJAD <sheikhabdullahamjadofficial@gmail.com>
2026-08-02 12:39:41 +0500

    Time: 0 ms (100%), Space: 2.2 MB (70.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.rs
+++ 0268-missing-number/0268-missing-number.rs
@@ -1,13 +1,17 @@
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let k = nums.iter();
-        let mut val = 0;
-        for i in 0..=*k.max().unwrap(){
-            val = i;
-            if !nums.contains(&i){
-                return i as i32;
-            } 
+        // Linear Complexity
+        // let k = nums.iter();
+        // let mut val = 0;
+        // for i in 0..=*k.max().unwrap(){
+        //     val = i;
+        //     if !nums.contains(&i){
+        //         return i as i32;
+        //     } 
+        // }
+        // return (val + 1) as i32
+
+        // better solve
+        return (0..=nums.len() as i32).sum::<i32>() - nums.iter().copied().sum::<i32>()        
         }
-        return (val + 1) as i32
-    }
 }
