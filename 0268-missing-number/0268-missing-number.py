c7ff3fdb74ae710920dc2339cc8c9d89c5f5e403
ABDULLAH AMJAD <sheikhabdullahamjadofficial@gmail.com>
2026-08-02 13:11:13 +0500

    Time: 0 ms (100%), Space: 20.4 MB (40%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        k = max(nums)
-        for i in (range(0, k+1)):
-            if i not in nums:
-                return i
-        return k+1
+        # Linear Complexity
+        # k = max(nums)
+        # for i in (range(0, k+1)):
+        #     if i not in nums:
+        #         return i
+        # return k+1
+        # Better One
+        return sum(range(len(nums)+1)) - sum(nums) 
