12bfd0980c0f251f4aea559a88361e62c096121d
ABDULLAH AMJAD <sheikhabdullahamjadofficial@gmail.com>
2026-08-16 18:01:55 +0500

    Time: 0 ms (100%), Space: 19.3 MB (22.29%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,11 +1,23 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        ret = [None]*len(nums)
-        for index, i in enumerate(nums):
-            k = 0
-            for j in nums:
-                if i > j:
-                    k +=1
-            ret[index] = k
-        print(ret)
-        return ret
+        # Big O n square
+        # ret = [None]*len(nums)
+        # for index, i in enumerate(nums):
+        #     k = 0
+        #     for j in nums:
+        #         if i > j:
+        #             k +=1
+        #     ret[index] = k
+        # print(ret)
+        # return ret
+        # I will write it for Big O n
+        d = {}
+
+
+        for i,v in enumerate(sorted(nums)):
+            if v not in d.keys():
+                d[v] = i 
+        
+        for index,k in enumerate(nums):
+            nums[index] = d[k]
+        return nums 
